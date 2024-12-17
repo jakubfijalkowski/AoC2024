5492bc41f828e57a684ec5bf79699d76fbdf2183
Jakub Fijałkowski <kuba@codinginfinity.me>
2024-12-17 10:19:08 +0100

    Update list extensions and add new libs/configs

--- lib/listExt.ml
+++ lib/listExt.ml
@@ -1,11 +1,36 @@
-open Either
-
 let all_predicate f = List.fold_left (fun acc x -> acc && f x) true
 
 let split_on_empty l =
-  let idx = List.find_index (fun x -> x = "") l |> Option.get in
-  let filtered = List.filter (fun x -> x <> "") l in
-  let mapped =
-    List.mapi (fun i x -> if i < idx then Left x else Right x) filtered
+  let acc, last =
+    List.fold_left
+      (fun (acc, curr) -> function
+        | "" -> (List.rev curr :: acc, [])
+        | x -> (acc, x :: curr))
+      ([], []) l
   in
-  List.partition_map (fun x -> x) mapped
+  List.rev (List.rev last :: acc)
+
+let sum = List.fold_left ( + ) 0
+
+let min_opt = function
+  | [] -> None
+  | lst -> Some (List.fold_left min max_int lst)
+
+let min1 = List.fold_left min max_int
+
+let min_by f lst =
+  let score, e =
+    List.fold_left
+      (fun (prev_min, prev_e) e ->
+        if f e < prev_min then (f e, Some e) else (prev_min, prev_e))
+      (max_int, None) lst
+  in
+  Option.map (fun e -> (score, e)) e
+
+let take n l =
+  let rec aux n acc = function
+    | [] -> List.rev acc
+    | x :: xs when n > 0 -> aux (n - 1) (x :: acc) xs
+    | _ -> List.rev acc
+  in
+  aux n [] l
